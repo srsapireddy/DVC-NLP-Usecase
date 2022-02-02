d82470a4b3bd3b41d74353452f626755f03dc99e
Srinivas Rahul Sapireddy <sapireddyrahul@gmail.com>
2022-02-01 23:26:25 -0600

    stage 01 tsv empty files created

--- src/stage_01_prepare.py
+++ src/stage_01_prepare.py
@@ -36,6 +36,17 @@ def main(config_path, params_path):
     prepared_data_dir_path = os.path.join(artifacts["ARTIFACTS_DIR"],artifacts["PREPARED_DATA"])
     create_directories([prepared_data_dir_path])
 
+    train_data_path = os.path.join(prepared_data_dir_path, artifacts["TRAIN_DATA"])
+    test_data_path = os.path.join(prepared_data_dir_path, artifacts["TEST_DATA"])
+
+    encoding = "utf8"
+    with open(input_data, encoding=encoding) as fd_in:
+        with open(train_data_path, "w", encoding=encoding) as fd_out_train:
+            with open(test_data_path, "w", encoding=encoding) as fd_out_test:
+                pass
+                # process_post(fd_in, fd_out_train, fd_out_test, "<python>", split)
+
+
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
     args.add_argument("--config", "-c", default="configs/config.yaml")
